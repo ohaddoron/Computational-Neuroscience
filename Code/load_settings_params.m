d96d9d56d981c96c88009358dbc26bcd20c75f8c
Ohad Doron <ogdoron@gmail.com>
2017-08-13 08:28:25 +0300

    Changes

--- Code/load_settings_params.m
+++ Code/load_settings_params.m
@@ -15,7 +15,7 @@ params.thresh = 20e-3; % membrane threshold [V]
 params.V0_exitatory = 16e-3; % membrane reset potential for exitatory neurons[V]
 params.V0_inhibatory = 13e-3; % memberane reset potential for inhibatory neurons [V]
 params.spike_potential = 30e-3; % memberane maximal potential at a spike [V]
-params.tau_exitatory = 15e-3; % membrane time constant [sec]
+params.tau_exitatory = 30e-3; % membrane time constant [sec]
 params.tau_inhibatory = 10e-3; % membrane time constant [sec]
 params.refract = 2e-3; % refractory period for exitatory neurons [sec]
 %% Network Parameters
@@ -23,12 +23,12 @@ params.f = 0.1; % Coding Level
 params.c = 0.2; % Probability of synaptic contact
 params.num_memories = 5; % number of memories in the simulation
 % params.num_neurons = (params.num_memories + 2) * params.num_neurons_group;
-params.num_inhibatory = 2e2; % num neurons
-params.num_exitatory = 8e2; % num neurons
-params.mu_ext_exitatotry = 23.1e-3; % extitory neurons mean external input [V]
-params.sigma_ext_exitatotry = 1e-3; % extitory neurons standard deviation external input [V]
-params.mu_ext_inhibatory = 21.0e-3; % inhibatory neurons mean external input [V]
-params.sigma_ext_inhibatory = 1e-3; % inhibatory neurons standard deviation external input [V]
+params.num_inhibatory = 2e3; % num neurons
+params.num_exitatory = 8e3; % num neurons
+params.mu_ext_exitatotry = 1e-3; % extitory neurons mean external input [V]
+params.sigma_ext_exitatotry = 1e-2; % extitory neurons standard deviation external input [V]
+params.mu_ext_inhibatory = 1e-3; % inhibatory neurons mean external input [V]
+params.sigma_ext_inhibatory = 1e-2; % inhibatory neurons standard deviation external input [V]
 params.num_neurons = params.num_inhibatory + params.num_exitatory; % total number of neurons in the simulation
 params.p_stimulation = 1e-3;
 %% Synaptic Parameters
@@ -47,11 +47,11 @@ params.tau_D = 200e-3; % Recovery time of synaptic resources [sec]
 params.tau_F = 1500e-3; % Recovery time of utilization factor [sec]
 params.tau_I_s = 300e-3; % Time constant of synaptic current
 %% Selective Stimulation 
-params.D_cue = 0e0; % Cue delay. Used to allow the network reach a spontaneous state
+params.D_cue = 0.5e0; % Cue delay. Used to allow the network reach a spontaneous state
 params.T_cue = 350e-3; % Duration of selective potentiation
-params.A_cue = 1.15e0; % Amplification factor
+params.A_cue = 4e0; % Amplification factor
 %% Reactivation Signal
-params.D_reactivating = 0.7e0; % Delay until reactivation
+params.D_reactivating = 1.5e0; % Delay until reactivation
 params.T_reactivating = 250e-3; % Duration of reactivating signal
 params.A_reactivating = 1.05e0; % Amplification factor
 %% Periodic activating signal
